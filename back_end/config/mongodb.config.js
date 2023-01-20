70241d5a5985382486ee9f9373332ac5851406bf
jittapat <idea26746@gmail.com>
2023-01-20 22:45:44 +0700

    ทำส่วน config เชื่อมกับ ฐานข้อมูล

---
+++ back_end/config/mongodb.config.js
@@ -0,0 +1,4 @@
+//ทำหน้าที่เก็บข้อมูลเเเละเชื่อมต่อกับ ฐานข้อมูล mongoDB
+module.exports ={
+    url: 'mongodb+srv://proideas:2103Ann2104@cluster01.5gdg5mg.mongodb.net/?retryWrites=true&w=majority'
+}
