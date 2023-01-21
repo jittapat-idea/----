9cca31de5e011a7d1f6a46b45d94c5850b1f136f
jittapat <idea26746@gmail.com>
2023-01-21 14:38:19 +0700

    เเก้ user model

--- back_end/model/users.js
+++ back_end/model/users.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose')
 
-const StudentSchema = mongoose.Schema(
-    {
-        StudentId: Number,
-        FullName: String,
-        Email: String
-    },
-    {
-        versionKey: false
-    }
-)
+const userSchema = mongoose.Schema({
+    UserID: Number,
+    FullName: String,
+    Email: String,
+    Password: String
+})
 
-const TeacherSchema = mongoose.Schema(
-    {
-        TeacherId: Number,
-        FullName: String,
-        Email: String
-    },
-    {
-        versionKey: false
-    }
-)
+module.exports = mongoose.model("user",userSchema);
 
-module.exports = {
-    Student: mongoose.model('Student_user',StudentSchema),
-    Teacher: mongoose.model('Teacher_user',TeacherSchema)
-}
+
+// const StudentSchema = mongoose.Schema(
+//     {
+//         StudentId: Number,
+//         FullName: String,
+//         Email: String
+//     },
+//     {
+//         versionKey: false
+//     }
+// )
+
+// const TeacherSchema = mongoose.Schema(
+//     {
+//         TeacherId: Number,
+//         FullName: String,
+//         Email: String
+//     },
+//     {
+//         versionKey: false
+//     }
+// )
+
+// module.exports = {
+//     Student: mongoose.model('Student_user',StudentSchema),
+//     Teacher: mongoose.model('Teacher_user',TeacherSchema)
+// }
