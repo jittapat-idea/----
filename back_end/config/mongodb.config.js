1921f7c0699d5b41ef33e12a8953f99deefe2088
jittapat <idea26746@gmail.com>
2023-01-21 14:18:03 +0700

    เเก้ config

--- back_end/config/mongodb.config.js
+++ back_end/config/mongodb.config.js
@@ -1,4 +1,11 @@
 //ทำหน้าที่เก็บข้อมูลเเเละเชื่อมต่อกับ ฐานข้อมูล mongoDB
-module.exports ={
-    url: 'mongodb+srv://proideas:2103Ann2104@cluster01.5gdg5mg.mongodb.net/?retryWrites=true&w=majority'
-}
+// module.exports ={
+//     url: 'mongodb+srv://proideas:2103Ann2104@cluster01.5gdg5mg.mongodb.net/?retryWrites=true&w=majority'
+// }
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://proideas:2103Ann2104@cluster01.5gdg5mg.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+    console.log("conected!!");
+}).catch((err)=>{
+    console.log("error");
+})
